1f0977b85d5b7159e5bcd2eca58b6ac47b5a5e79
Lonmar Wolf <51096012+EUMARLON@users.noreply.github.com>
2025-06-24 14:12:26 -0300

    att

--- main.js
+++ main.js
@@ -149,6 +149,7 @@ ${t.map((r,o)=>`${o+1}) ${r.toString()}`).join(`
   justify-items: center; /* Centraliza os itens */
 }
   .produto {
+  border-radius: 12px;
   display: flex;
   flex-direction: column;
   align-items: center;
